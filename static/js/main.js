b3af057ae05573b29514eb197e437d181411d75f
maaa <stigmas-offers0n@icloud.com>
2024-03-09 00:39:24 +0100

    :3

---
+++ static/js/main.js
@@ -0,0 +1,39 @@
+let appGrid = document.getElementById("appGrid")
+
+async function getStore(link) {
+    const response = await fetch(link);
+    const data = await response.json();
+    return data
+}
+
+getStore("repo/store.json").then((data) => {
+    console.log(data["title"])
+    console.log(data["apps"])
+    for (index in data["apps"]) {
+        let application = data["apps"][index]
+
+        // UI stuff
+        let appDiv = document.createElement("div")
+        appDiv.classList.add("app")
+        appGrid.append(appDiv)
+
+        let appImage = document.createElement("img")
+        appImage.src = application["iconURL"]
+        appDiv.append(appImage)
+
+        let appTitle = document.createElement("p")
+        appTitle.classList.add("title")
+        appTitle.innerText = application["name"]
+        appDiv.append(appTitle)
+
+        let appInfo = document.createElement("p")
+        appInfo.classList.add("w300")
+        appInfo.innerText = application["developerName"] + " | " + application["subtitle"]
+        appDiv.append(appInfo)
+
+        let appButton = document.createElement("button")
+        appButton.innerText = "Get"
+        appDiv.append(appButton)
+    }
+    //console.log(data)
+});
